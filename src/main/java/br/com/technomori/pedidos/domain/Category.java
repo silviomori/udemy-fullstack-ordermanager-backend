dc6c3f9c8e3017c6ff178bdf0dbb5a7430eb944e
Silvio Mori Neto <silviomori@gmail.com>
2018-05-31 15:31:05 -0300

    Testing the first domain class: Category (using Lombok to generate boilerplates).

---
+++ src/main/java/br/com/technomori/pedidos/domain/Category.java
@@ -0,0 +1,13 @@
+package br.com.technomori.pedidos.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Category {
+
+	private int id;
+	private String name;
+
+}
