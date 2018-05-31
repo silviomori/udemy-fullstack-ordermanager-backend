d84f4631bd241726ef3b10a5edb089ac7de0dbc5
Silvio Mori Neto <silviomori@gmail.com>
2018-05-31 17:26:12 -0300

    Populating Category with some data. Creating constructors to Category.

--- src/main/java/br/com/technomori/ordermanager/OrderManagerApplication.java
+++ src/main/java/br/com/technomori/ordermanager/OrderManagerApplication.java
@@ -1,12 +1,31 @@
 package br.com.technomori.ordermanager;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class OrderManagerApplication {
+import br.com.technomori.ordermanager.domain.Category;
+import br.com.technomori.ordermanager.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class OrderManagerApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 }
--- src/main/java/br/com/technomori/ordermanager/domain/Category.java
+++ src/main/java/br/com/technomori/ordermanager/domain/Category.java
@@ -5,16 +5,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private String name;
 
 }
