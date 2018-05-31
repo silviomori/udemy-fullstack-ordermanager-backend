147c2c27b726cb0540f2817a4a55fccee4f51eae
Silvio Mori Neto <silviomori@gmail.com>
2018-05-31 18:48:38 -0300

    Product Entity and Repository + Relationship ManyToMany with Category.

--- src/main/java/br/com/technomori/ordermanager/OrderManagerApplication.java
+++ src/main/java/br/com/technomori/ordermanager/OrderManagerApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.technomori.ordermanager.domain.Category;
+import br.com.technomori.ordermanager.domain.Product;
 import br.com.technomori.ordermanager.repositories.CategoryRepository;
+import br.com.technomori.ordermanager.repositories.ProductRepository;
 
 @SpringBootApplication
 public class OrderManagerApplication implements CommandLineRunner {
@@ -16,16 +18,40 @@ public class OrderManagerApplication implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagerApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Category cat1 = new Category(null, "Computing");
-		Category cat2 = new Category(null, "Office");
+		Category computingCategory = Category.builder().name("Computing").build();
+		Category officeCategory = Category.builder().name("Office").build();
 		
-		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoryRepository.saveAll(Arrays.asList(computingCategory,officeCategory));
+		
+		
+		Product computer = Product.builder()
+				.name("Computer")
+				.price(2000d)
+				.category(computingCategory)
+				.build();
+		Product printer = Product.builder()
+				.name("Printer")
+				.price(800d)
+				.category(computingCategory)
+				.category(officeCategory)
+				.build();
+		Product mouse = Product.builder()
+				.name("Mouse")
+				.price(80d)
+				.category(computingCategory)
+				.build();
+		
+		productRepository.saveAll(Arrays.asList(computer,printer,mouse));
 		
 	}
 }
--- src/main/java/br/com/technomori/ordermanager/domain/Product.java
+++ src/main/java/br/com/technomori/ordermanager/domain/Product.java
@@ -1,7 +1,6 @@
 package br.com.technomori.ordermanager.domain;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,29 +8,26 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Singular;
 
 @Entity
 @Data
 @NoArgsConstructor
-@AllArgsConstructor(access=AccessLevel.PRIVATE)
+@AllArgsConstructor
 @Builder
-public class Category {
+public class Product {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-
 	private String name;
-
-	@ManyToMany(mappedBy="categories")
-	@Setter(value=lombok.AccessLevel.PRIVATE)
-	@Builder.Default
-	private Collection<Product> products = new ArrayList<Product>();
-
+	private Double price;
+	
+	@ManyToMany
+	@Singular
+	private List<Category> categories;
 }
