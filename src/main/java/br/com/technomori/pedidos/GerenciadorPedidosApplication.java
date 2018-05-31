420537b232956b2ef11431c629e2535e87b1ff19
Silvio Mori Neto <silviomori@gmail.com>
2018-05-31 13:12:14 -0300

    Initial Commit - Creating the OrderManager project.

---
+++ src/main/java/br/com/technomori/pedidos/GerenciadorPedidosApplication.java
@@ -0,0 +1,12 @@
+package br.com.technomori.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorPedidosApplication.class, args);
+	}
+}
