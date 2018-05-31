7e9beae3d6bc975f032b23269e5a35026db53294
Silvio Mori Neto <silviomori@gmail.com>
2018-05-31 16:01:57 -0300

    H2 Database creation with Category Entity/Table using Spring Data JPA

--- src/main/java/br/com/technomori/pedidos/domain/Category.java
+++ src/main/java/br/com/technomori/pedidos/domain/Category.java
@@ -1,13 +1,23 @@
 package br.com.technomori.pedidos.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Entity
 @Data
 @AllArgsConstructor
 public class Category {
 
-	private int id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
 	private String name;
 
 }
